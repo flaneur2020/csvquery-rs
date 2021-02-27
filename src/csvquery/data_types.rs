209f1cbe89612bc22421f773c3e971e86ef5666d
Li Yazhou <liyazhou@fenbi.com>
2021-02-27 23:29:58 +0800

    data_types: add column vector

--- src/csvquery/data_types.rs
+++ src/csvquery/data_types.rs
@@ -1,6 +1,6 @@
+use crate::csvquery::scalar::ScalarValue;
 use arrow;
 use arrow::array::ArrayRef;
-use crate::csvquery::scalar::ScalarValue;
 
 pub type DataType = arrow::datatypes::DataType;
 pub type DataField = arrow::datatypes::Field;
@@ -12,4 +12,34 @@ pub type RecordBatch = arrow::record_batch::RecordBatch;
 pub enum ColumnVector {
     Array(ArrayRef),
     Scalar(ScalarValue),
+}
+
+impl ColumnVector {
+    pub fn data_type(&self) -> DataType {
+        match self {
+            ColumnVector::Array(arr) => self.array_data_type(arr.clone()),
+            ColumnVector::Scalar(s) => self.scalar_data_type(s),
+        }
+    }
+
+    fn array_data_type(&self, arr: ArrayRef) -> DataType {
+        arr.data_type().clone()
+    }
+
+    fn scalar_data_type(&self, s: &ScalarValue) -> DataType {
+        match s {
+            ScalarValue::Boolean(_) => DataType::Boolean,
+            ScalarValue::Float32(_) => DataType::Float32,
+            ScalarValue::Float64(_) => DataType::Float64,
+            ScalarValue::Int8(_) => DataType::Int8,
+            ScalarValue::Int16(_) => DataType::Int16,
+            ScalarValue::Int32(_) => DataType::Int32,
+            ScalarValue::Int64(_) => DataType::Int64,
+            ScalarValue::UInt8(_) => DataType::UInt8,
+            ScalarValue::UInt16(_) => DataType::UInt16,
+            ScalarValue::UInt32(_) => DataType::UInt32,
+            ScalarValue::UInt64(_) => DataType::UInt64,
+            ScalarValue::Utf8(_) => DataType::Utf8,
+        }
+    }
 }
